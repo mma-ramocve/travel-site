4696161e8f21f6213e59bd705e663d49f48aa921
mma-ramocve <aleksandra1kovacevic@gmail.com>
2020-02-18 16:14:00 +0100

    updated cloud function...

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,27 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the secret area"
-    }) 
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Here we can <strong>learn</strong> something</p>
+    `
+    
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        }) 
+    } else {
+        callback(null, {
+            statusCode: 401
+        }) 
+    }
+
+
 }
